0e752b17680915ab1c6afb45b49345bbf6e06cf8
Van Hoang <111828642+vhoang11@users.noreply.github.com>
2023-03-05 23:12:13 -0600

    started profile page

---
+++ pages/profile.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Image from 'next/image';
+import { Button } from 'react-bootstrap';
+import { useAuth } from '../utils/context/authContext';
+import { signOut } from '../utils/auth';
+
+export default function UserProfile() {
+  const { user } = useAuth();
+
+  return (
+    <div style={{ marginTop: '35px' }}>
+      <Image src={user.photoURL} alt="userURL" width="200px" height="200px" />
+      <h1>Name: {user.displayName}</h1>
+      <h2>Email: {user.email}</h2>
+      <h3>Last Login: {user.metadata.lastSignInTime}</h3>
+      <Button variant="danger" onClick={signOut}> Sign Out</Button>
+    </div>
+  );
+}
