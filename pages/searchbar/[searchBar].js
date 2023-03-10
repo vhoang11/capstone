4a23830c4393eba784ba122916d70da917f9a655
Van Hoang <111828642+vhoang11@users.noreply.github.com>
2023-03-09 21:13:52 -0600

    search bar working

--- pages/searchbar/[searchBar].js
+++ pages/searchbar/[searchBar].js
@@ -3,6 +3,7 @@
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import { getTutorials } from '../../api/tutorialData';
+import TutorialCard from '../../components/TutorialCard';
 
 export default function SearchBar() {
   const [searchTutorials, setSearchTutorials] = useState([]);
@@ -12,7 +13,7 @@ export default function SearchBar() {
 
   const searchAllTutorials = () => {
     getTutorials().then((tutorials) => {
-      const filteredTutorials = tutorials.filter((tutorial) => tutorial.tutorial.toLowerCase().includes(searchBar) || tutorial.name.toLowerCase().includes(searchBar) || tutorial.timestamp.includes(searchBar));
+      const filteredTutorials = tutorials.filter((tutorial) => tutorial.title.toLowerCase().includes(searchBar) || tutorial.description.toLowerCase().includes(searchBar) || tutorial.timestamp.includes(searchBar));
 
       setSearchTutorials(filteredTutorials);
     });
@@ -27,8 +28,8 @@ export default function SearchBar() {
 
   return (
     <>
-      <div>
-        {searchTutorials.map((aim) => <tutorialCard key={aim.firebaseKey} tutorialObj={aim} onUpdate={searchAllTutorials} />)}
+      <div className="d-flex flex-wrap text-center" id="search-results" style={{ marginTop: '100px', padding: '30px', backgroundColor: 'rgba(255,255,255, 0.5)' }}>
+        {searchTutorials.map((tutorial) => <TutorialCard key={tutorial.firebaseKey} tutorialObj={tutorial} onUpdate={searchAllTutorials} />)}
       </div>
     </>
   );
